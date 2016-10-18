7c6f74570760c830d09de67ae05c23a5ccc1108c
vipin-gm <vipin.verma@gyanmatrix.com>
2016-10-18 15:37:03 +0530

    implemented start/stop to slider

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -1,10 +1,34 @@
 angular.module('newsapp') 
 
-.controller('homeCtrl', function($scope, lastestNews, $ionicSlideBoxDelegate) {
-	
-	console.log("hello form home");	
-	console.log(lastestNews)
-	$scope.headlines = lastestNews; 
+.controller('homeCtrl', function($scope, lastestNews, $ionicSlideBoxDelegate, $timeout) {
 
-	$ionicSlideBoxDelegate.update();
+	$scope.sliderStatus = true;
+
+	$timeout(function() {
+
+		$scope.headlines = lastestNews; 
+
+		 // $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
+		 $ionicSlideBoxDelegate.update();
+	},1000);
+
+	$scope.toggleSliderStatus = function() {
+
+		if ($scope.sliderStatus) {
+
+			$ionicSlideBoxDelegate.$getByHandle('news-viewer').stop();	
+		}
+
+		else {
+
+			$ionicSlideBoxDelegate.$getByHandle('news-viewer').start();	
+		}
+
+		$scope.sliderStatus = !$scope.sliderStatus;
+	};
+
+	// $ionicSlideBoxDelegate.update();
+	// $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
+ //    },2000)	
+
 });
